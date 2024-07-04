99f9705f0f48860f14ee2882931fbef06eadee54
Manish Kumar <jangramanish2602@gmail.com>
2024-07-04 11:03:25 +0530

    same element

---
+++ array/sameele.java
@@ -0,0 +1,24 @@
+package array;
+
+public class sameele {
+    public static void main(String args[])
+    {
+        int[] arr = {8,0,3,5,0,2,0};
+        int [] b = new int[arr.length];
+    
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=0)
+            {
+                b[i]=arr[i];
+                 
+            }
+            
+            System.out.println(b[i]);
+        }
+    } 
+
+}
+//container with most water
+
+//leader element
