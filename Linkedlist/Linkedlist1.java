0bc3dec415933d2964daf1a3f3a648b1d285fc47
Manish Kumar <jangramanish2602@gmail.com>
2024-07-08 10:55:52 +0530

    creation of linkedlist

---
+++ Linkedlist/Linkedlist1.java
@@ -0,0 +1,40 @@
+package Linkedlist;
+
+public class Linkedlist1 {
+
+    public static void main(String[] args) {
+
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+
+
+        Node head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        display(head);
+    }
+
+        public static void display(Node head)
+        {
+            Node curr = head;
+            while(curr!=null)
+            {
+                System.out.print(curr.data+" ");
+                curr=curr.next;
+            }
+        }
+    }
+       
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data)
+    {
+        this.data = data;
+    }
+}
