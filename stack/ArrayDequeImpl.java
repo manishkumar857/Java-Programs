2e48a3315f209d5da8c9a61a1c0c2b162526c689
Manish Kumar <jangramanish2602@gmail.com>
2024-07-11 10:26:48 +0530

    Array Deque implementation

---
+++ stack/ArrayDequeImpl.java
@@ -0,0 +1,22 @@
+package stack;
+import java.util.ArrayDeque;
+
+public class ArrayDequeImpl{
+    public static void main(String[] args) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        while ((!stack.isEmpty())) {
+
+            System.out.println(stack.pop());
+            
+        }
+
+    }
+}
