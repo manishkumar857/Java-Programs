c33da63b2351ee334f616ac660ee1645c0819486
Manish Kumar <jangramanish2602@gmail.com>
2024-07-21 22:37:54 +0530

    tree

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -1,38 +1,46 @@
 package Tree;
+//from parent node various child nodes comes out and there is no cyclic order.
+//height is measured form down to top and vicecersa for depth ,but for whole tree both is same.
+//representation/image of tree is state space of trees
 
+
+import java.util.*;
 public class Tree {
-    //from parent node various child nodes comes out and there is no cyclic order.
-    //height is measured form down to top and vicecersa for depth ,but for whole tree both is same.
-    // representation/image of tree is state space of trees
-
+    Scanner sc;
+    
+    public Tree()
+    {
+        this.sc= new Scanner(System.in); 
+    }
     
     public static void main(String[] args) {
-        //Node node1 = new Node(10);
-        //Node node2 = new Node(20);
-        //Node node3 = new Node(30);
-        //Node root = node1;
-        //root.left=node2;
-        //root.right=node3;
-
-
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-        Node node4 = new Node(4);
-        Node node5 = new Node(5);
-        Node node6= new Node(6);
-        Node node7 = new Node(7);
-        Node root =node1;
-        root.left=node2;
-        root.right=node3;
-        root.left.left=node4;
-        root.left.right=node5;
-        root.right.left=node6;
-        root.right.right=node7; 
-        
-         
+        Tree tb = new Tree();  
+        System.out.println("Enter Node data");
+        Node root = tb.createbinarytree();
+    
     }
-    class Node{
+
+    public Node createbinarytree()
+    {
+        System.out.println("Enter Node data");
+        int data = sc.nextInt();
+        if(data==-1)
+        {
+            return null;
+        }
+        Node node = new Node(data);
+        System.out.println("enter data "+data+" left side");
+        node.left=createbinarytree();
+
+        System.out.println("enter data "+data+" right side");
+        node.right=createbinarytree();
+
+        return node;
+       
+
+
+    }
+    static class Node{
         int data;
         Node left;
         Node right;
@@ -40,5 +48,6 @@ public class Tree {
         {
             this.data=data;
         }
+        
     }
 }
---
+++ Tree/inorder.java
@@ -0,0 +1,15 @@
+package Tree;
+import java.util.*;
+public class inorder {
+
+    Scanner scanner;
+
+    public inorder(){
+        scanner=new Scanner(System.in);
+    }
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
