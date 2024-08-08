24993c1c1215a1a119c75d04f2b224f36bfaae38
Manish <jangramanish2602@gmail.com>
2024-08-08 14:00:44 +0530

    iterative seach in Linked List

---
+++ Linkedlist/iterativesearch.java
@@ -0,0 +1,103 @@
+package Linkedlist;
+
+public class iterativesearch {
+  
+    public static class Node{
+    int data;
+    Node next;
+    
+    public Node(int data)
+    {
+        this.data = data;
+        this.next=null;
+    }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void AddFirst(int data)
+    {
+        //step 1 create new node
+        Node newNode = new Node(data);
+        if(head==null)
+        {
+            head=tail=newNode;
+            return;
+
+        }
+
+        //step 2 newNode next = head
+        newNode.next=head;
+        //step 3 point head to new Node
+
+        head = newNode;
+
+    }
+
+    public  void AddLast(int data)
+    {
+        Node newNode = new Node(data);
+        if(head==null)
+        {
+            head=tail=newNode;
+            return;
+
+        }
+        tail.next = newNode;
+        tail = newNode;
+
+    }
+
+    public void display()
+    {
+        if(head == null)
+        {
+            System.out.println("linked list is empty");
+        }
+        Node temp = head;
+        while (temp!=null) {
+
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+            if(temp == null)
+            System.out.println("null");
+            
+        }
+    }
+
+    public int itsearch(int key)
+    {
+        int i=0;
+        Node temp =head;
+        while(temp!=null)
+        {
+            if(temp.data==key)
+            {
+                
+                return i;
+            }
+            temp=temp.next;
+            i++;
+
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+
+       iterativesearch ll = new iterativesearch();
+       ll.AddFirst(2);
+       ll.AddFirst(4);
+       ll.AddLast(5);
+       ll.AddLast(7);
+
+       ll.display();
+       System.out.println(ll.itsearch(5));
+       System.out.println(ll.itsearch(11));
+    }
+}
+
+
+       
+
+
